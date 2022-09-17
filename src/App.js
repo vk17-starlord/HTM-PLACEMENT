d2190c4b091ef400ba6110581a8cb5caa4a595ee
vinit <khollam.vinit@gmail.com>
2022-09-17 16:39:54 +0530

    merge me

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from './pages/LandingPage';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp'
+import DashBoard from './pages/DashBoard';
+import JobDetail from './pages/JobDetail';
+import ProfilePage from './pages/ProfilePage'
+import CompanyProfile from './pages/CompanyProfile';
 function App() {
   return (
     <div className="App">
       <Routes>
-                
         <Route path="/" element={<LandingPage />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path='/Job/:jobid' element={<JobDetail/>}/>
+        <Route path='/student/:id' element={<ProfilePage />} />
+        <Route path="/company/:id" element={<CompanyProfile />} />
       </Routes>
     </div>
   );
