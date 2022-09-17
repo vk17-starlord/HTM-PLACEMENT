e40fecd2e76cdcbcd97c844d2873d65d900349bf
vinit <khollam.vinit@gmail.com>
2022-09-17 12:29:56 +0530

    added profile page component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path='/student/id:' element={<ProfilePage />} />
+        <Route path='/student/:id' element={<ProfilePage />} />
         <Route path="/company/:id" element={<CompanyProfile />} />
       </Routes>
     </div>
--- src/pages/CompanyProfile.jsx
+++ src/pages/CompanyProfile.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useParams } from 'react-router-dom';
 function CompanyProfile() {
   const { id } = useParams();
-
+  
   return (
     <div className='w-full min-h-screen'>
      
