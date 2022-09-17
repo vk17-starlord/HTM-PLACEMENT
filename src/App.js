efe7e84fb64364d02c72fd12b3268a0ad1be34e1
vinit <khollam.vinit@gmail.com>
2022-09-17 12:25:53 +0530

    blahbblah

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import LandingPage from './pages/LandingPage';
-
+import CompanyProfile from './pages/CompanyProfile';
 function App() {
   return (
     <div className="App">
       <Routes>
-                
         <Route path="/" element={<LandingPage />} />
+        <Route path="/company/:id" element={<CompanyProfile />} />
       </Routes>
     </div>
   );
---
+++ src/pages/CompanyProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+function CompanyProfile() {
+  const { id } = useParams();
+
+  return (
+    <div className='w-full min-h-screen'>
+     
+    </div>
+  )
+}
+
+export default CompanyProfile
